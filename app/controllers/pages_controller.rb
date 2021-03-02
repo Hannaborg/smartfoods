e6ac23fe31f0afcbd816a2c9eb31bf2ec26b0064
Hanna Borg <hanna.r.borg@gmail.com>
2021-03-02 18:39:46 +0100

    merge

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,11 +1,5 @@
 class PagesController < ApplicationController
-  # skip_before_action :authenticate_user!, only: :home
-
   def home
   end
 
-  # def dashboard
-  #   @user = current_user
-  #   @goals = @user.goals
-  # end
 end
