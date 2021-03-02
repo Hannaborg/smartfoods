d1a65f8edc249f5408280f7a4025532a1855340b
Alexandre Coelho <alexandre.mmc97@gmail.com>
2021-03-02 17:16:29 +0000

    foods and shopping list controllers

--- app/controllers/shopping_lists_controller.rb
+++ app/controllers/shopping_lists_controller.rb
@@ -1,10 +1,21 @@
 class ShoppingListsController < ApplicationController
   def index
+    @shopping_lists = ShoppingList.all
   end
 
   def create
+    puts params
+    @shopping_list = ShoppingList.create(shopping_list_params)
   end
 
   def destroy
+    puts params
+    ShoppingList.find(params[:shopping_list]).destroy
+  end
+
+  private
+
+  def shopping_list_params
+    params.require(:shopping_list).permit(:id)
   end
 end
