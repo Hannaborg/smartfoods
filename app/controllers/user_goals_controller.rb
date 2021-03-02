4be36712b14b33052b7da2a29e240679da7cf31e
Hanna Borg <hanna.r.borg@gmail.com>
2021-03-02 16:22:36 +0100

    user_goals controller and view

--- app/controllers/user_goals_controller.rb
+++ app/controllers/user_goals_controller.rb
@@ -1,10 +1,21 @@
 class UserGoalsController < ApplicationController
   def index
+    @goals = Goal.all
   end
 
   def new
+    @user = current_user
+    @goal = Goal.new
   end
 
   def create
+    @goal = Goal.new
+    @goal.user = current_user
+    if @goal.save
+      redirect_to goal_path(@goal)
+    else
+      render :new
+    end
   end
+
 end
