045c8dbf438b5c29d6c48f73a24b6d60b4178a54
jose queiroz <zemqueiroz@gmail.com>
2021-03-03 16:08:41 +0000

    favorites

--- app/controllers/favorite_recipes_controller.rb
+++ app/controllers/favorite_recipes_controller.rb
@@ -1,17 +1,18 @@
 class FavoriteRecipesController < ApplicationController
   def index
+    @user = current_user
     @favorite_recipes = FavoriteRecipe.all
   end
 
   def create
-    @favorite_recipe = FavoriteRecipe.new(favorite_params)
     @favorite_recipe.user = current_user
+    @favorite_recipe = FavoriteRecipe.new(favorite_params)
     if @favorite_recipe.save
       flash[:success] = "Recipe successfully saved"
-      redirect_to  food_recipes_path
+      redirect_to food_recipes_path
     else
       flash[:error] = "Something went wrong"
-      redirect_to  food_recipe_path
+      redirect_to food_recipe_path
     end
   end
 
@@ -29,6 +30,6 @@ class FavoriteRecipesController < ApplicationController
   private
 
   def favorite_params
-    params.require(:favorite_recipe).permit()
+    params.require(:favorite_recipe).permit(:recipe_id, :user_id)
   end
 end
