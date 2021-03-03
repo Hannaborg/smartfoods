b36a9fcd7bf8c5816ac6634d9e5d6c399b4f29b6
Hanna Borg <hanna.r.borg@gmail.com>
2021-03-03 15:52:30 +0100

    no big changes

--- config/routes.rb
+++ config/routes.rb
@@ -3,9 +3,9 @@ Rails.application.routes.draw do
   devise_for :users
   root to: 'pages#home'
 
-  resources :foods, only: %i[ index ] do  
+  resources :foods, only: %i[ index ] do
     resources :recipes, only: %i[ index show ] do
-      
+
   end
 end
 resources :recipes, only: %i[] do
@@ -14,4 +14,5 @@ end
   resources :favorite_recipes, only: %i[ index destroy ]
   resources :shopping_lists, only: %i[ index create destroy ]
   resources :user_goals, only: %i[ index create new ]
+  resources :user_views, only: %i[ index ]
 end
