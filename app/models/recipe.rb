c2509ba8fcda0af75a52dc59f837ebe0db1654b4
jose queiroz <zemqueiroz@gmail.com>
2021-03-08 16:36:09 +0000

    remove button working

--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -1,7 +1,7 @@
 class Recipe < ApplicationRecord
   has_many :recipe_foods
   has_many :foods, through: :recipe_foods
-  has_many :favorite_recipes
+  has_many :favorite_recipes, dependent: :destroy
   has_many :users, through: :favorite_recipes
 
   validates :title, :description, presence: true
